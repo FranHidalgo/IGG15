203aeb042b137ba68f32f98ff86d520ad6bb5996
javierfuenca <javierfuenca@gmail.com>
2016-05-14 21:15:09 +0200

    Elegir fecha por mes
    Incluye elegir fecha por mes

--- ISST-G15-ControlGlucosa/war/js/jquery.mtz.monthpicker.js
+++ ISST-G15-ControlGlucosa/war/js/jquery.mtz.monthpicker.js
@@ -35,7 +35,7 @@
  */
 /**
  * GPL LIcense
- * Copyright (c) 2011, Luciano Costa
+ * Copyright (c) 2011, Luciano Costa 
  * 
  * This program is free software: you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the 
